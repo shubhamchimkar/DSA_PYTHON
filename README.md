872ba0bd09ad36704f86c5e28756c930a78f6dc2
Shubham Chimkar <shubhamchimkar5@gmail.com>
2024-11-28 12:46:09 +0530

    readme file updated

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # DSA_PYTHON
+This repository contains some basic data structures and algorithms implemented in Python:
+
+* [Array](array_ops.py)
+* [Cuboid](cuboid.py)
+* [Data Types](data_types.py)
+* [Dictionary](dictionary.py)
+* [List](lists.py)
+* [List Comprehension](list_compre.py)
+* [Queue](queue_ops.py) (using array)
+* [Stack](stack_ops.py) (using array)
+* [Tuples](tuples.py)
+* [Time-Tuples](time-tuple.py)
+
+This repository is for learning purpose. All the codes are tested and verified. The codes are written in a simple way so that it can be easily understood by the beginners. The codes are written with proper comments and docstrings for better understanding. The codes are tested in Python 3.8.
+
--- queue_ops.py
+++ queue_ops.py
@@ -1,32 +1,63 @@
 import array
 
+"""
+Class Queue:
+    Implementing the queue data structure
+"""
+
+
 class Queue:
+    """
+    Queue Class
+    """
     def __init__(self):
+        """
+        Constructor
+        """
         self.my_queue = array.array('i', [])
         self.front = -1
 
+
     def get_front(self):
+        """
+        Returns the front element of the Queue
+        """
         if self.is_empty():
             print("Queue is empty, no front element.")
         else:
             print(self.my_queue[0])
 
+
     def get_rear(self):
+        """
+        Returns the rear element of the Queue
+        """
         if self.is_empty():
             print("Queue is empty, no rear element.")
         else:
             print(self.my_queue[-1])
 
+
     def is_empty(self):
+        """
+        Checks whether the Queue is empty or not
+        """
         if len(self.my_queue):
             return False
         else:
             return True
 
+
     def enqueue(self, element):
+        """
+        Adds an element to the rear of the Queue
+        """
         self.my_queue.append(element)
     
     def dequeue(self):
+        """
+        Removes an element from the front of the Queue
+        """
         if self.is_empty():
             print("Queue is empty can't dequeue")
         else:
@@ -52,3 +83,4 @@ if __name__ == "__main__":
     
     print(queue.my_queue)
     queue.dequeue()
+
